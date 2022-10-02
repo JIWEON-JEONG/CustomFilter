4b5f08c0556dc7b9c8e1afe8d7559c3da7a75446
JIWEON-JEONG <dlektl6@naver.com>
2022-10-02 21:45:59 +0900

    remove test

--- src/test/java/STUDY/CUSTOM/TilApplicationTests.java
+++ src/test/java/STUDY/CUSTOM/TilApplicationTests.java
@@ -1,15 +1,15 @@
-package STUDY.CUSTOM;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
-
-@SpringBootTest
-class TilApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package STUDY.CUSTOM;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.Date;
+//
+//@SpringBootTest
+//class TilApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
